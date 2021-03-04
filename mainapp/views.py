5877b0bc706df9fbe2de1bbd0394103e329763bb
Nekregor <nekrasov.e.g@yandex.ru>
2021-03-04 19:08:55 +0300

    lesson 1

---
+++ mainapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
+
+
