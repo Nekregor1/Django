aac2a1e7f00dcc375a0eb0b11688faef8fb07ce0
nekregor <nekrasov.e.g@yandex.ru>
2021-03-19 21:50:26 +0300

    lesson 4 complete

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from authapp.models import ShopUser
 
@@ -11,4 +12,38 @@ class ShopUserLoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-            
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'email', 'first_name', 'age', 'avatar', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'\
+                
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data <18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'email', 'first_name', 'age', 'avatar', 'password')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            if field_name == 'password':
+                field.widget = forms.HiddenInput()
+    
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data <18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
--- authapp/views.py
+++ authapp/views.py
@@ -1,15 +1,64 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 # Create your views here.
 
 
 def login(request):
-    
+    print(request.POST)
+    login_form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+
     content = {
         'title': 'вход',
+        'login_form': login_form
     }
     return render(request, 'authapp/login.html', content)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
+
+
+def register(request):
+
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+    
+    content = {
+        'title': 'регистрация',
+        'form': register_form
+    }
+    return render(request, 'authapp/register.html', content)
+
+def edit(request):
+    
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+    
+    content = {
+        'title': 'редактирование',
+        'edit_form': edit_form
+    }
+
+    return render(request, 'authapp/edit.html', content)
