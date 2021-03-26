c222e7eeedee3f77de2a232b57a08bcc874f2aab
nekregor <nekrasov.e.g@yandex.ru>
2021-03-26 15:24:36 +0300

    final fix lesson 6

--- static/js/basket_script.js
+++ static/js/basket_script.js
@@ -1,20 +1,16 @@
 window.onload = function() {
-
-    // добавляем ajax-обработчик для обновления количества товара
-    $('.basket_list').on('click', 'input' [type = "number"], function() {
+    $('.basket_list').on('click', 'input[type="number"]', function() {
         let target_href = event.target;
 
-        if (target_href) {
-            $.ajax({
-                url: "/basket/edit/" + target_href.name + "/" + target_href.value + "/",
+        $.ajax({
+            url: "/basket/edit/" + target_href.name + "/" + target_href.value + "/",
 
-                success: function(data) {
-                    $('.basket_list').html(data.result);
-                    console.log('ajax done');
-                },
-            });
-        }
+            success: function(data) {
+                $('.basket_list').html(data.result);
+                console.log('ajax done');
+            },
+        });
+
         event.preventDefault();
     });
-
-};
+}
