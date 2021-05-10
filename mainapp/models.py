43b1c4491dfaebda1f19f9c256d31896964cf8b7
nekregor <nekrasov.e.g@yandex.ru>
2021-05-10 16:25:48 +0300

    Убираем неактивные продукты с формы (урок 5)

--- mainapp/models.py
+++ mainapp/models.py
@@ -23,4 +23,9 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.category.name})'
+
+    @staticmethod
+    def get_items():
+       return Product.objects.filter(is_active=True).\
+                              order_by('category', 'name')
     
