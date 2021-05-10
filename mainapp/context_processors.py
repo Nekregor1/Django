25d7b3c2d39629beaa0b4bb147bbfc772236c158
nekregor <nekrasov.e.g@yandex.ru>
2021-05-10 16:11:11 +0300

    siege test update

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -3,7 +3,8 @@ from basketapp.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = Basket.objects.filter(user=request.user)
+        # basket_list = Basket.objects.filter(user=request.user)
+        basket_list = Basket.get_items(request.user).select_related()
     return {
         'basket': basket_list
     }
